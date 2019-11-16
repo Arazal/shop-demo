79616220fe75660c861b0ce3b81a75d05849c38c
Adel <azalaradle@gmail.com>
2019-11-16 12:40:53 +0300

    Updates

---
+++ precache-manifest.aa5b3176bad1c24a2fa781952a68f459.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "10b6a2189706054cb8cc53ea9c751b8f",
+    "url": "/shop-demo/index.html"
+  },
+  {
+    "revision": "b0876058097a63cbff13",
+    "url": "/shop-demo/static/css/main.544f0fe0.chunk.css"
+  },
+  {
+    "revision": "92bbd48baba55c257651",
+    "url": "/shop-demo/static/js/2.817e3050.chunk.js"
+  },
+  {
+    "revision": "b0876058097a63cbff13",
+    "url": "/shop-demo/static/js/main.6b01c673.chunk.js"
+  },
+  {
+    "revision": "b3813092e5b9dbd2b573",
+    "url": "/shop-demo/static/js/runtime-main.a33e6252.js"
+  },
+  {
+    "revision": "19ef7b17bc9414c63c7781251ce0d24d",
+    "url": "/shop-demo/static/media/crown.19ef7b17.svg"
+  },
+  {
+    "revision": "30f0707118fb960665491424c115ac44",
+    "url": "/shop-demo/static/media/shopping-bag.30f07071.svg"
+  }
+]);
